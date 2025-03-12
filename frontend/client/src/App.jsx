0853c9911168924c2e3e692154e7eb77bba3b234
akshit-sharma-15 <asharma889452@gmail.com>
2025-03-12 10:58:52 +0530

    done

---
+++ frontend/client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LandingPage from "./pages/landingpage";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
