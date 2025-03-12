841d8b28615ac10b777431bf5584899139519aa6
akshit-sharma-15 <asharma889452@gmail.com>
2025-03-12 12:48:31 +0530

    done

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import LandingPage from "./pages/landingpage";
+import Chaicard from './components/chaiwalaCard' // Import ChaiwalaCard
 import "./App.css";
+
+const dummyChaiwala = {
+  name: "Sharma Ji Ki Chai",
+  location: "Mumbai, India",
+  rating: 4.8,
+  image: "https://images.hindi.news18.com/ibnkhabar/uploads/2021/10/733738_FEATURE20210930_185024.jpg",
+};
 
 function App() {
   return (
     <div className="app-container">
       <LandingPage />
+      <div className="chaiwala-section">
+        <h2>Top Chaiwalas</h2>
+        <Chaicard{...dummyChaiwala} />
+      </div>
     </div>
   );
 }
 
 export default App;
+
